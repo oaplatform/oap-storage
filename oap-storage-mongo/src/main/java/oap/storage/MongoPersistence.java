2f9d9c82882b4c1c1c2360aeffd1149ecfd2b5a2
alebastrov <alebastrov@mailinator.com>
2022-12-29 10:18:18 +0200

    speed up

--- oap-storage-mongo/src/main/java/oap/storage/MongoPersistence.java
+++ oap-storage-mongo/src/main/java/oap/storage/MongoPersistence.java
@@ -89,10 +89,10 @@ public class MongoPersistence<I, T> implements Closeable {
     private final Path crashDumpPath;
     @ServiceName
     public String serviceName;
-    public boolean watch = false;
+    public volatile boolean watch = false;
     protected int batchSize = 100;
     private ExecutorService watchExecutor;
-    private ScheduledExecutorService scheduler;
+    private volatile ScheduledExecutorService scheduler;
     private volatile long lastExecuted = -1;
 
     public MongoPersistence( MongoClient mongoClient, String collectionName, long delay, MemoryStorage<I, T> storage ) {
@@ -123,11 +123,9 @@ public class MongoPersistence<I, T> implements Closeable {
     public void preStart() {
         log.info( "collection = {}, fsync delay = {}, watch = {}, crashDumpPath = {}",
             collectionName, Dates.durationToString( delay ), watch, crashDumpPath );
-
+        scheduler = oap.concurrent.Executors.newScheduledThreadPool( 1, serviceName );
         synchronizedOn( lock, () -> {
             this.load();
-
-            scheduler = oap.concurrent.Executors.newScheduledThreadPool( 1, serviceName );
             scheduler.scheduleWithFixedDelay( this::fsync, delay, delay, TimeUnit.MILLISECONDS );
         } );
 
@@ -216,7 +214,7 @@ public class MongoPersistence<I, T> implements Closeable {
             fsync();
             log.debug( "closed {}...", this );
         } );
-        else log.debug( "this {} was't started or already closed", this );
+        else log.debug( "this {} wasn't started or already closed", this );
 
         if( watch ) Closeables.close( watchExecutor );
     }
